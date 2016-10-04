683ef42be850fdf0d55b45e15c7a7dff96eb5c56
Ryan Fanshaw <ryanmfanshaw@gmail.com>
2016-10-04 19:13:25 +0200

    updated default config, added directories

--- zp/config.py
+++ zp/config.py
@@ -1,4 +1,4 @@
-UFID_LENGTH	= 20
+UFID_LENGTH	= 10
 
 PHASH_SIZE	= 15
 DHASH_SIZE	= 15
@@ -11,3 +11,17 @@ SMALL_THUMB_SIZE		= 100
 LARGE_THUMB_QUALITY		= 95
 MEDIUM_THUMB_QUALITY	= 80
 SMALL_THUMB_QUALITY		= 20
+
+# Absolute paths, use trailing slashes
+DIR_MAIN		= '/mnt/library/code/zurfa-photos/'
+DIR_IMAGES		= '/mnt/library/data/'
+DIR_LIBRARY		= '/mnt/library/data/library/'
+DIR_IMPORT		= '/mnt/library/data/import/'
+DIR_PROCESSED	= '/mnt/library/data/processed/'
+DIR_THUMBS		= '/mnt/library/data/thumbs/'
+DIR_THUMBS_L	= '/mnt/library/data/thumbs/large/'
+DIR_THUMBS_M	= '/mnt/library/data/thumbs/medium/'
+DIR_THUMBS_S	= '/mnt/library/data/thumbs/small/'
+DIR_THUMBS_SQ	= '/mnt/library/data/thumbs/square/'
+
+DIR_PROC_FORMAT	= "import_%Y%m%d_%H%M%S/"
