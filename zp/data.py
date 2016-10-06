207bdebe5e622f99341ff5f775876c44da392f5d
Ryan Fanshaw <ryanmfanshaw@gmail.com>
2016-10-06 16:43:00 +0200

    restarting data module using SQLAlchemy - working add_to_library

--- zp/data.py
+++ zp/data.py
@@ -1,54 +1,70 @@
-import sqlite3 as lite
-import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Numeric, LargeBinary
+from sqlalchemy.orm import scoped_session, sessionmaker
+import sqlalchemy.exc
+import time
+import config
+import logger
+
+
+engine  = create_engine('sqlite:///%s' % config.DATABASE, echo=False)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base    = declarative_base()
+Base.query = db_session.query_property()
+
+lg = logger.Logger()
+lg.setup(__name__)
+
+class Library(Base):
+    __tablename__   = 'library'
+
+    id          = Column(Integer, primary_key=True)
+    ufid        = Column(String(10), unique=True)
+    path        = Column(String(128))
+    origin      = Column(String(64))
+    size        = Column(Integer)
+    checksum    = Column(String(40))
+    added       = Column(Integer)
+    updated     = Column(Integer, default=int(time.time()))
+    extension   = Column(String(7))
+    format      = Column(String(10))
+    category    = Column(String(10))
+    taken       = Column(Integer)
+    lat         = Column(Numeric(15,11))
+    lon         = Column(Numeric(15,11))
+    device      = Column(String(32))
+    width       = Column(Integer)
+    height      = Column(Integer)
+    exif_dump   = Column(LargeBinary)
+
+class Hashes(Base):
+    __tablename__   = 'hashes'
+
+    id          = Column(Integer, primary_key=True)
+    ufid        = Column(String(10), unique=True)
+    phash       = Column(String(56))
+    dhash       = Column(String(56))
+    whash       = Column(String(64))
 
 class Data(object):
     """docstring for Data"""
     def __init__(self):
         super(Data, self).__init__()
-        self.db = SQLLite()
 
-    def add_to_library(self,data):
-        pass
-
-class SQLLite(object):
-    """docstring for SQLLite"""
-    def __init__(self):
-        super(SQLLite, self).__init__()
-        self.conn    = None
-
-    def add_to_library(self,data):
-        data    = self.noner(data)
-        library_insert_query = ("INSERT INTO `library` (`id`, `ufid`, `path`, `origin`, `size`, "
-            "`checksum`, `added`, `updated`, `extension`, `format`, `category`, `taken`, "
-            "`lat`, `lon`, `device`, `width`, `height`, `exif_dump`) "
-            "VALUES (NULL, :ufid, :path, :origin, :size, :checksum, :added, :updated, :extension, "
-            ":format, :category, :taken, :lat, :lon, :device, :width, :height, :exif_dump);")
-        if not self.conn:
-            self.connect()
-        cur = self.conn.cursor()
+    def add_to_library(self,data,Close=True):
+        # library = session.query(Library)
         try:
-            cur.execute(library_insert_query,data)
-        except lite.Error as Err:
-            print Err
+            item    = Library(**data)
+            db_session.add(item)
+            db_session.commit()
+        except sqlalchemy.exc.IntegrityError:
+            lg.logger.error("Error inserting item into the database")
+            return False
         else:
-            cur.close()
-
-    def commit(self):
-        self.conn.commit()
-
-
-    def connect(self):
-        if not self.conn:
-            self.conn = lite.connect('library.db')
-
-    def noner(self,data_in):
-        columns = ["ufid", "path", "origin", "size", "checksum", "added", "updated", 
-            "extension", "format", "category", "taken", "lat", "lon", "device",
-             "width", "height", "exif_dump"]
-        data_out   = {}
-        for column in columns:
-            if column in data_in:
-                data_out.update({column:data_in[column]})
-            else:
-                data_out.update({column:None})
-        return data_out
+            return True
+        finally:
+            if Close:
+                db_session.remove()
