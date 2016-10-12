f6d7acc37a5b3406c19d1c5dad8c42e4a215d79e
Ryan Fanshaw <ryanmfanshaw@gmail.com>
2016-10-12 20:39:25 +0200

    added relationships model

---
+++ zp/models/relationships.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+# zp.core
+from zp.core.database import Base
+
+class Library(Base):
+    """SQLAlchemy schema for 'relationships' table"""
+    __tablename__   = 'relationships'
+
+    id          = Column(Integer, primary_key=True)
+    primary     = Column(String(10))
+    secondary   = Column(String(10))
+    type        = Column(String(1))
+    value       = Column(Integer)
+
+    def __init__(self, primary=None, secondary=None, type=None, value=None):
+        self.primary    = primary
+        self.secondary  = secondary
+        self.type       = type
+        self.value      = value
+        
