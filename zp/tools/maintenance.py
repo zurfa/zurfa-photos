3e251364447a0089824a291b2cc4a816434c0e9e
Ryan Fanshaw <ryanmfanshaw@gmail.com>
2016-11-04 22:53:40 +0100

    added thumbnails table to populate_auxiliary_tables function

--- zp/tools/maintenance.py
+++ zp/tools/maintenance.py
@@ -1,11 +1,14 @@
 from zp import db, app
-from zp.core.models import Items, Metadata, Hashes
+from zp.core.models import Items, Metadata, Hashes, Thumbs
+import zp.core.thumbnail as thumb
+import zp.core.config as config
 
 
 def populate_auxiliary_tables():
     items_table = Items.query.add_columns(Items.ufid).all()
     metadata_table = Metadata.query.add_columns(Metadata.ufid).all()
     hashes_table = Hashes.query.add_columns(Hashes.ufid).all()
+    thumbs_table = Thumbs.query.add_columns(Thumbs.ufid).all()
 
     # items = map(lambda item: dict((i, item.__dict__[i])
     #                                  for i in item.__dict__ if i != '_sa_instance_state'), items_table)
@@ -17,17 +20,17 @@ def populate_auxiliary_tables():
     items    = map(lambda item: item[1], items_table)
     metadata = map(lambda item: item[1], metadata_table)
     hashes   = map(lambda item: item[1], hashes_table)
+    thumbs   = map(lambda item: item[1], thumbs_table)
 
-    metadata_missing = []
-    hashes_missing = []
+    missing = []
     for item in items:
         if not item in metadata:
-            # metadata_missing.append({'ufid': item})
-            metadata_missing.append(Metadata(ufid=item))
+            missing.append(Metadata(ufid=item))
         if not item in hashes:
-            # hashes_missing.append({'ufid': item})
-            hashes_missing.append(Hashes(ufid=item))
+            missing.append(Hashes(ufid=item))
+        if not item in thumbs:
+            missing.append(Thumbs(ufid=item))
 
-    db.session.bulk_save_objects(metadata_missing)
-    db.session.bulk_save_objects(hashes_missing)
+    db.session.bulk_save_objects(missing)
     db.session.commit()
+
